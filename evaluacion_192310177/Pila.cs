0e93102db6faeb6a5a49f6621281b68e2cdc5778
Andrea Evelyn Mejía Rubio <andiemartin33@gmail.com>
2021-02-27 02:59:48 -0600

    Agregar archivos de proyecto.

---
+++ evaluacion_192310177/Pila.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evaluacion_192310177
+{
+    class Pila
+    {
+      
+       //se declara el numero de elementos para declarar un arreglo que se llama pilas
+       const int numeroElementos = 10;
+       private int[] pila = new int[numeroElementos];
+
+        //declaracion de variables
+        int maximoPila = numeroElementos - 1;
+        int Tope;
+
+        bool ValidacionLleno()
+        {
+            //aqui valido que la pila no este llena para poder seguir insertando elementos
+            if(Tope == maximoPila)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void insertarPila(int dato)
+        {
+
+
+            if (ValidacionLleno())
+            {
+                //tope va sumando una posicion cada que se inserta un dato a la pila
+                Tope++;
+                pila[Tope] = dato;
+            }
+
+            else
+            {
+                //se muestra un mensaje de que la pila esta llena
+                Console.WriteLine("La pila esta llena");
+            }
+
+
+        }
+
+      
+
+        public void mostrarElemento ()
+        {
+            Console.WriteLine("El elemento {0} en la pila es: {1}", Tope, pila[Tope]);
+            Tope--;
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ evaluacion_192310177/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace evaluacion_192310177
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // se declaran las variables
+            int valor=1;
+            int nCantidad = 0;
+            string elemento;
+
+            //se declara la pila
+            Stack pila = new Stack();
+
+
+            Console.WriteLine("**************************** PILA ****************************************");
+            Console.WriteLine("--------------------------------------------------------------------------------------");
+
+            //el usuario introduce cuantos elementos quiere que le pida para ingresar
+            Console.WriteLine("Cuantos elementos quieres ingresar: ");
+            nCantidad = int.Parse(Console.ReadLine());
+            Console.WriteLine("--------------------------------------------------------------------------------------");
+            
+
+            for (int i = 0; i < nCantidad; i++)
+            {
+                //ingresa el valor el usuario y con la propiedad push se inserta a la pila y esta dentro de un for para que suceda dicha insercion 
+                Console.WriteLine(" ");
+                Console.WriteLine("Ingrese el elemento {0}: " , valor++);
+                elemento = Console.ReadLine();
+                pila.Push(elemento);
+            }
+
+            //se muestra el contenido de la pila mediante un foreach 
+            Console.WriteLine("---------------------------------------------------------------------------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine("Mostrar pila");
+            Console.WriteLine(" ");
+
+            foreach (string var in pila)
+            {
+                Console.WriteLine(var);
+            }
+
+            //peek retorna el valor que esta al tope de la pila sin eliminarlo
+            Console.WriteLine("---------------------------------------------------------------------------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine("El tope de la pila es: ");
+            Console.WriteLine(" ");
+            Console.WriteLine(pila.Peek());
+            Console.WriteLine("--------------------------------------------------------------------------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
+    
+
